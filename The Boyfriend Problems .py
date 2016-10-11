3316ae5371b38185a61152bcb523e3fc909a5616
Saiful Bashar <robal.bashar59@gmail.com>
2016-10-11 13:35:38 +0600

    problem

--- The Boyfriend Problems .py
+++ The Boyfriend Problems .py
@@ -1,4 +1,9 @@
 case = int(input())
 for i in range(case):
     n = int(input())
+    arr = list(input().split())
+    arr = list(map(int,arr))
+    
+    print ("".join([str(x)+" " for x in arr] ))
+    
     
